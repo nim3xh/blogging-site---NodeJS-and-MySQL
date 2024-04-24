79ceaebdb3796487becf8e7a5bb1f6ed115c1894
nim3xh <nim3xh@gmail.com>
2024-04-24 16:23:01 +0530

    Add show and index routes for posts

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -23,6 +23,39 @@ function save(req, res) {
     });
 }
 
+function show(req, res) {
+    // show logic
+    const id=req.params.id;
+    models.Post.findByPk(id).then(result=>{
+        if(result){
+            res.status(200).json(result);
+        }else{
+            res.status(404).json({
+                message:"Post not found"
+            });
+        }
+    }).catch(error=>{
+        res.status(500).json({
+            message:"Something went wrong",
+            error:error
+        });
+    });
+}
+
+function index(req, res) {
+    // index logic
+    models.Post.findAll().then(result=>{
+        res.status(200).json(result);
+    }).catch(error=>{
+        res.status(500).json({
+            message:"Something went wrong",
+            error:error
+        });
+    });
+}
+
 module.exports = {
-    save: save
+    save: save,
+    show: show,
+    index: index
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,7 @@ const postsController = require('../controllers/post.controller'); // Import pos
 const router = express.Router(); // Create a router
 
 router.post('/',postsController.save); // Handle GET request at /posts
+router.get('/:id',postsController.show); // Handle GET request at /posts/:id
+router.get('/',postsController.index); // Handle GET request at /posts
 
 module.exports = router; // Export the router
